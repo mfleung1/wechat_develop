ec2db8f943a7db4a116a23e8406d6d3ac1ec7cc7
mf.leung <mf.leung@glassesgallery.com>
2018-11-19 12:48:24 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+App({
+  onLaunch: function (options) {
+    // Do something initial when launch.
+    //console.log(options)
+  },
+  onShow: function (options) {
+    // Do something when show.
+  },
+  onHide: function () {
+    // Do something when hide.
+  },
+  onError: function (msg) {
+    console.log(msg)
+  },
+  globalData: 'I am global data'
+})
